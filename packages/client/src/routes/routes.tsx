710deebc59133f15e4ad241fd4f1557431137455
Valeriia <valeriya_ekhno@traffic1.ru>
2026-02-27 16:15:30 +0300

    FEATURE: add register page (SI-5.10)

--- packages/client/src/routes/routes.tsx
+++ packages/client/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { initMainPage, MainPage } from '../pages/Main'
 import { initFriendsPage, FriendsPage } from '../pages/FriendsPage'
 import { initNotFoundPage, NotFoundPage } from '../pages/NotFound'
+import { initRegisterPage, RegisterPage } from '../pages/register/Register'
 
 export const routes = [
   {
@@ -12,6 +13,11 @@ export const routes = [
     path: '/friends',
     Component: FriendsPage,
     fetchData: initFriendsPage,
+  },
+  {
+    path: '/register',
+    Component: RegisterPage,
+    fetchData: initRegisterPage,
   },
   {
     path: '*',
