beb4b3ad85b64511fa90cd1cf1cb311285e296e6
Valeriia <valeriya_ekhno@traffic1.ru>
2026-02-27 16:09:15 +0300

    FEATURE: add login page (SI-5.6)

--- packages/client/src/routes/routes.tsx
+++ packages/client/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { initMainPage, MainPage } from '../pages/Main'
 import { initFriendsPage, FriendsPage } from '../pages/FriendsPage'
 import { initNotFoundPage, NotFoundPage } from '../pages/NotFound'
+import { initLoginPage, LoginPage } from '../pages/login/Login'
 
 export const routes = [
   {
@@ -12,6 +13,11 @@ export const routes = [
     path: '/friends',
     Component: FriendsPage,
     fetchData: initFriendsPage,
+  },
+  {
+    path: '/login',
+    Component: LoginPage,
+    fetchData: initLoginPage,
   },
   {
     path: '*',
