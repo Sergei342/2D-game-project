80cf7bbfd850d98bf49fe6d55fe51161c879da33
irina_shabliy <Irina.Rozova@constructor.ru>
2026-03-01 00:08:54 +0300

    add as develop

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
-
 import { routes } from './routes/routes'
+
 import 'antd/dist/reset.css'
 import { ConfigProvider, theme } from 'antd'
 import { theme as appTheme } from './config/theme'
@@ -12,8 +12,7 @@ import { GlobalStyles } from './styles/styles'
 
 const router = createBrowserRouter(routes)
 
-ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
+createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <GlobalStyles />
     <ConfigProvider
