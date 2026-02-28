60176f53b4dbc8a62cf0e9340beb40617bff0e5b
sergei <dukmas08@gmail.com>
2026-02-28 17:21:37 +0300

    fix script yarn dev and hydrate method

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
-
 import { routes } from './routes'
 
 const router = createBrowserRouter(routes)
 
-ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
+createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
