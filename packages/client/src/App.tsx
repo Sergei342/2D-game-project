d20d3fe68230389364985818c38d85b851ce4f07
sergei <dukmas08@gmail.com>
2026-04-12 19:29:55 +0300

    fix: simplify ssr app markup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
-
 const App = () => {
   return (
-    <main className="app">
+    <main>
       <h1>Server Side Rendering</h1>
       <p>This page was rendered on the Express server.</p>
+      <p>
+        {
+          '\u041f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044c \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d!'
+        }
+      </p>
     </main>
   )
 }
